7c5abd4375cdcce7159c598c8b587cb519c5c4c8
Athar Ahmad Mahmood <atharahmadmahmood@gmail.com>
2023-07-26 22:36:25 +0500

    bar fixed

--- Assets/_MyProject/Scripts/Gameplay/MeltedIceCreamHandler.cs
+++ Assets/_MyProject/Scripts/Gameplay/MeltedIceCreamHandler.cs
@@ -42,10 +42,11 @@ public class MeltedIceCreamHandler : MonoBehaviour
         //{
         //    return;
         //}
+            Debug.Log("Melt size" + size);
         size += meltingSpeed;
         //UpdateVisual();
 
-        if (myTransform.localScale.y >= .9f)
+        if (myTransform.localScale.y >= .7f)
         {
             Debug.Log("Drown");
             Routine.WaitAndCall (0.5f, () => GamePlayManager.Instance.Drown());
